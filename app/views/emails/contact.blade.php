d85aea77d969ba73f7c5c60e55637f643ad8b3fd
charlie <charlie@charliemcr.com>
2017-10-14 14:29:10 +0100

    Removes all project files

--- app/views/emails/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<p>Date: {{ date("F j, Y, g:i a")}}</p>
-<p>Name: {{ Input::get('name')}}</p>
-<p>Email: {{ Input::get('email')}}</p>
-<p>Message: {{ Input::get('message')}}</p>
-
-<p>IP: {{ Request::getClientIp()}}</p>
