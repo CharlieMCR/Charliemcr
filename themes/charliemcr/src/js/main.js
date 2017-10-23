1234b34b917d28f191c78f3877497dc61d601685
charlie <charlie@charliemcr.com>
2017-10-23 18:53:44 +0100

    Updates document ready event listener to work with fast pages

--- themes/charliemcr/src/js/main.js
+++ themes/charliemcr/src/js/main.js
@@ -1,42 +1,45 @@
 import styles from './../css/main.css';
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    let form = document.querySelector('form');
+document.onreadystatechange = function () {
 
-    if (form && form.length) {
+    if (document.readyState === "complete") {
+        let form = document.querySelector('form');
 
-        form.addEventListener('submit', function (e) {
-            let formData = new FormData(form);
-            let result = {};
-            let xhr = new XMLHttpRequest();
-            let message = document.querySelector('.md p');
+        if (form && form.length) {
+            form.addEventListener('submit', function (e) {
+                let formData = new FormData(form);
+                let result = {};
+                let xhr = new XMLHttpRequest();
+                let message = document.querySelector('.md p');
 
-            e.preventDefault();
+                e.preventDefault();
 
-            if (formData.get('_gotcha') && formData.get('_gotcha').length) {
-                return;
-            }
-            formData.delete('_gotcha');
-            if (formData.get('email')) {
-                formData.append('_replyto', formData.get('email'));
-            }
-            for (let entry of formData.entries())
-            {
-                result[entry[0]] = entry[1];
-            }
-            xhr.open(form.method, form.action, true);
-            xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-            xhr.onreadystatechange = function () {
-                if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                    form.reset();
-                    message.innerText = "Thanks for the message, I'll be in touch soon.";
-                    message.classList.add('green');
-                } else {
-                    message.innerText = "Something went wrong.";
-                    message.classList.add('red');
+                if (formData.get('_gotcha') && formData.get('_gotcha').length) {
+                    return;
                 }
-            };
-            xhr.send(JSON.stringify(result));
-        });
+                formData.delete('_gotcha');
+                if (formData.get('email')) {
+                    formData.append('_replyto', formData.get('email'));
+                }
+                for (let entry of formData.entries())
+                {
+                    result[entry[0]] = entry[1];
+                }
+                xhr.open(form.method, form.action, true);
+                xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                xhr.onreadystatechange = function () {
+                    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                        form.reset();
+                        message.innerText = "Thanks for the message, I'll be in touch soon.";
+                        message.classList.add('green');
+                    } else {
+                        message.innerText = "Something went wrong.";
+                        message.classList.add('red');
+                    }
+                };
+                xhr.send(JSON.stringify(result));
+            });
+        }
     }
-});
+
+};
